a5df29f04d90db412a9792b0b9e6b115c8860f83
Harwin <sandhuharwinkaur@gmail.com>
2025-09-18 00:13:14 +1000

    finished the project

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-const taskList = [];
+let taskList = [];
+
+const hoursPerWeek = 24 * 7;
+
 const handleOnSubmit = (e) => {
     // const elm = document.getElementById("task");
     // console.log(elm.value);
 
 const newForm = new FormData(e); //FormData is built-in browser constructor
 const task = newForm.get("task");
-const hr = newForm.get("hr");
+const hr = +newForm.get("hr");
 
 const obj = {
     task: task,
     hr: hr,
     id: randomIdGenerator(),
+    type: "entry"
+}
+
+//check if there is enough hours left
+
+const existingTtlHrs = taskTotal()
+
+if(existingTtlHrs + hr > hoursPerWeek){
+    return alert("Sorry not enough time fit this task from last week");
 }
 
 // console.log(task);
@@ -36,8 +48,8 @@ const displayEntryList = () => {
                 <td>${item.task}</td>
                 <td>${i.hr}</td>
                 <td class="text-end">
-                    <button class="button btn btn-danger"> <i class="fa-solid fa-trash"></i></button>
-                    <button class="button btn btn-success"> <i class="fa-solid fa-arrow-right"></i></button>
+                    <button type="button" onclick="handleOnDelete('${item.id}')" class="button btn btn-danger"> <i class="fa-solid fa-trash"></i></button>
+                    <button type="button" onclick="switchTask('${item.id}', 'bad')" class="button btn btn-success"> <i class="fa-solid fa-arrow-right"></i></button>
                 </td>
                 </tr>`;
     });
@@ -47,6 +59,33 @@ const displayEntryList = () => {
     // console.log(taskList);
     // console.log("first");
 }
+
+
+const displayBadList = () => {
+    
+    let str = "";
+
+    console.log(taskList);
+    const badElm = document.getElementById("badList");
+
+    const badList = taskList.filter((item) => item.type === "bad");
+
+    badList.map((item, i) => {
+        str +=`<tr>
+                <td>${i + 1}</td>
+                <td>${item.task}</td>
+                <td>${i.hr}</td>
+                <td class="text-end">
+                <button type="button" onclick="switchTask('${item.id}', 'entry')" class="button btn btn-warning"> <i class="fa-solid fa-arrow-left"></i></button>
+                    <button type="button" onclick="handleOnDelete('${item.id}')" class="button btn btn-danger"> <i class="fa-solid fa-trash"></i></button>
+                </td>
+                </tr>`;
+    });
+
+    badElm.innerHTML = str;
+    document.getElementById('savedHrsElm').innerText = badList.reduce((acc, item) => acc + item.hr, 0);
+};
+
 
 //generating unique id
 const randomIdGenerator = (length = 6) => {
@@ -60,4 +99,32 @@ const randomIdGenerator = (length = 6) => {
          id += str[randomIndex];
     }
     return id;
+};
+
+const handleOnDelete = (id) => {
+if(window.confirm("Are you sure to delete?")){
+    taskList = taskList.filter((item) => item.id !== id);
+    displayEntryList();
+    displayBadList();
+   }
+}
+const switchTask = (id, type) =>{
+    taskList = taskList.map((item) => {
+        if(item.id === id){
+            item.type = type;
+        }
+        return item;
+    }); 
+
+    displayEntryList();
+    displayBadList();
+};
+
+const taskTotal = () => {
+    const ttlHr = taskList.reduce((acc, item) => {
+        return acc + item.hr;
+    }, 0);   
+
+ document.getElementById('ttlHrs').innerText = ttlHr;
+ return ttlHr;
 };
